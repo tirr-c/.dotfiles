0375ba601d4dd07bb3f144c1127666a840ea7f08
VBChunguk <chwo9843@gmail.com>
2017-03-03 14:30:23 +0900

    Add installation script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Working directory check
+if [ "$(pwd)" != "$HOME" ]; then
+  echo "Run the script at your home directory."
+  exit 1
+fi
+
+# Dependencies
+DEPS="zsh curl"
+for dep in $DEPS; do
+  echo "Dependency: $dep"
+  which $dep >/dev/null 2>&1
+  if [ ! $? ]; then
+    echo "Please install $dep."
+    exit 1
+  fi
+done
+
+# zplug/zplug
+echo "Installing zplug"
+curl -sL zplug.sh/installer | zsh
+
+# junegunn/vim-plug
+echo "Installing vim-plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Symlinking
+BASEDIR="$(dirname "$0")"
+DOTFILES=".zshrc .vimrc .tmux.conf"
+for dotfile in $DOTFILES; do
+  echo "Symlinking $dotfile"
+  ln -sf "$BASEDIR/$dotfile"
+done
