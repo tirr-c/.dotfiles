123f3be795f991fdb7f4b927035073fc7df08417
Wonwoo Choi <chwo9843@gmail.com>
2021-06-23 16:44:57 +0900

    .p10k.zsh: move status code segment

--- .p10k.zsh
+++ .p10k.zsh
@@ -33,6 +33,7 @@
     # =========================[ Line #1 ]=========================
     context                 # user@hostname
     # os_icon               # os identifier
+    status                  # exit code of the last command
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
@@ -46,7 +47,6 @@
   # last prompt line gets hidden if it would overlap with left prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    status                  # exit code of the last command
     # command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     # direnv                  # direnv status (https://direnv.net/)
